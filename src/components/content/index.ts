7d4d11e9eabbc3840e0d575f56b9af3a68189ee4
Muhammad Faqih Akmal <62086734+AqilAkmal@users.noreply.github.com>
2022-06-27 23:11:51 +0800

    [feat] Add modal to index

--- src/components/content/index.ts
+++ src/components/content/index.ts
@@ -5,15 +5,17 @@ import { registerComponent } from "../../utils/plugins";
 import DataTable from "./DataTable.vue";
 import FloatingCard from "./FloatingCard.vue";
 import OutlinedCard from "./OutlinedCard.vue";
+import Modal from "./Modal.vue";
 
 const Plugin = {
   install(app: App) {
     registerComponent(app, DataTable);
     registerComponent(app, FloatingCard);
     registerComponent(app, OutlinedCard);
+    registerComponent(app, Modal);
   }
 }
 
 export default Plugin;
 
-export { DataTable, FloatingCard, OutlinedCard };
+export { DataTable, FloatingCard, OutlinedCard, Modal };
