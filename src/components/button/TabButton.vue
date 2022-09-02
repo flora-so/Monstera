4cf9344532e7f67baf0c8fd28b518df69c736f29
Ong Zheng Kai <ozk.zhengkai@gmail.com>
2022-09-02 10:41:24 +0800

    [wip] adding tabs

---
+++ src/components/button/TabButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="pt-2 bg-primary-light rounded-t-lg">
+    <span class="text-lg font-bold px-6 text-primary">
+      Users
+    </span>
+    <div class="mt-1 pt-1 bg-primary rounded-t"></div>
+  </div>
+
+  <div class="pt-2 rounded-t-lg">
+    <span class="text-lg font-semibold px-6 text-gray">
+      Invites
+    </span>
+    <!-- <div class="mt-1 pt-1 bg-gray rounded-t"></div> -->
+  </div>
+</template>
